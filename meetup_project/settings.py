9a1d252f962ef21eb9ddd3f0147e4f20347fe55f
alvinjiang32 <36828072+alvinjiang32@users.noreply.github.com>
2021-04-28 12:28:28 -0700

    created staticfiles for client to pull

--- meetup_project/settings.py
+++ meetup_project/settings.py
@@ -17,7 +17,8 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DEBUG_PROPAGATE_EXCEPTIONS = True
+SESSION_COOKIE_SCURE = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
