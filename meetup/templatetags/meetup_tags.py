2e71c413600e8ba2bc536fd02f49983ca7ca38a7
Sam <sam.vuong@sjsu.edu>
2021-04-29 00:38:46 -0700

    Digits of credit card are now hidden except for last 4.

--- meetup/templatetags/meetup_tags.py
+++ meetup/templatetags/meetup_tags.py
@@ -12,3 +12,8 @@ def get_wallet(**filters):
 @register.filter(name="has_group")
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+   
+ 
+@register.filter
+def index(indexable, i):
+    return indexable[i]
